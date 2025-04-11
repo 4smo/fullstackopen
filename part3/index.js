8e2387fd08f8c0cfd3dc844e49f6bdfe016a3173
asmo <asmo.koivula@gmail.com>
2025-04-11 15:53:43 +0300

    fixed error handling on the frontend

--- part3/index.js
+++ part3/index.js
@@ -104,6 +104,9 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
+  if (error.name == 'ValidationError') {
+    return response.status(400).json({ error: error.message})
+  }
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
